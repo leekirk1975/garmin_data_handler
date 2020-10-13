4e7cea08e77888a6e3dd3fa45895463eb0625076
leekirk1975 <leekirk1975@me.com>
2020-10-13 21:04:20 +0800

    new filed added

--- Garmin_test.py
+++ Garmin_test.py
@@ -2,10 +2,25 @@
 
 import garminexport
 import garminconnect as gc
+from datetime import date 
 
 email, pwd  = open('GamrinDetails.txt').read().strip().split(',')#set the client secret and refresh token
 
+#set todays date
+today = date.today()
+
 client = gc.Garmin(email,pwd)
+gc.Garmin.login(client)
+
+user = gc.Garmin.get_full_name(client)
+print(user)  
+  
+units = gc.Garmin.get_unit_system(client)
+print(units)
+
+sleepdata =  gc.Garmin.get_sleep_data(client,today.isoformat())
+print(sleepdata)
 
 
-print(gc.Garmin.get_full_name(client))
+act = gc.Garmin.get_activities(client,0,1)
+print(act)
