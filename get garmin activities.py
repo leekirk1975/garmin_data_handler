8a7ac5bbb641443457bdb69a795ecc7fadd33ae6
leekirk1975 <leekirk1975@me.com>
2020-11-07 15:56:03 +0800

    new filed added

--- get garmin activities.py
+++ get garmin activities.py
@@ -12,10 +12,8 @@ email, pwd  = open('GamrinDetails.txt').read().strip().split(',')
 #set todays date
 today = datetime.date.today()
 
-
-
-client = gcb.GarminClient(email,pwd)
-gcb.GarminClient.connect(client)
-gcb.incremental_backup()
+cwd = os.getcwd()
+backup_dir = cwd + '/data/garmin_backup'
+gcb.incremental_backup(email,pwd,backup_dir,'ALL', False,7)
 
 
