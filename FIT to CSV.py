16f0c852bd24dce68bccd515a239df861a2cb607
leekirk1975 <leekirk1975@me.com>
2021-01-09 21:44:01 +0800

    add fit to panadas

--- FIT to CSV.py
+++ FIT to CSV.py
@@ -32,29 +32,48 @@ fitfile = FitFile(file)
 # include:
 # exclude: file_id,'file_creator','unknown','device_settings','user_profile','zones_target'
 fit_record_type = ['record'] #'hrv','event', 'sport']
-
+workout_id = file[-14:-4]
 
 for record_type in fit_record_type:
     list_workouts = []
+    list_labels = ['start', 'end']
+
     for record in fitfile.get_messages(record_type):
         # Go through all the data entries in this record
         workout_dict = {}
         for record_data in record:
 
             # Print the records name and value (and units if it has any)
-
             if record_data.units and 'unknown' not in record_data.name:
                 print(" * %s: %s %s" % (
-                    record_data.name, record_data.value, record_data.units,
-                ))
-                workout_dict[record_data.name] = record_data.value
+                    record_data.name, record_data.value, record_data.units))
+
+                if isinstance(record_data.value, (list,tuple)):
+
+                    for item, label  in zip(record_data.value, list_labels):
+                        workout_dict[record_data.name + '_' + label] = item
+                else:
+                    workout_dict[record_data.name] = record_data.value
+
             elif 'unknown' not in record_data.name:
                 print(" * %s: %s" % (record_data.name, record_data.value))
-                workout_dict[record_data.name] = record_data.value
 
+                if isinstance(record_data.value, (list,tuple)):
+                    for item, label in zip(record_data.value, list_labels):
+                        workout_dict[record_data.name + '_' + label] = item
+
+                else:
+
+                    workout_dict[record_data.name] = record_data.value
+
+        workout_dict['workout ID'] = workout_id
         list_workouts.append(workout_dict)
         print()
 
     df_workouts = pd.DataFrame(list_workouts)
 
 print(df_workouts)
+
+# Save all data files to a sub directory to aviod cultering
+csvname = cwd + '/data/' + workout_id + '.csv'
+df_workouts.to_csv(csvname, index=False)
