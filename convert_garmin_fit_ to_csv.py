9bf031ce6c8b375f279ebe607e6aa380afa5d701
leekirk1975 <leekirk1975@me.com>
2021-01-12 22:12:11 +0800

    add fit to panadas

--- convert_garmin_fit_ to_csv.py
+++ convert_garmin_fit_ to_csv.py
@@ -9,55 +9,48 @@ import get_garmin_health_data as ggh
 # file_id, event, sport, record, HRV, session
 cwd = os.getcwd()  # get the current working directory
 # Note The file name needs to be reformatted from to 2020-12-04T21:22:38+00:00_5913788595.fit'
-file = cwd + '/data/garmin_backup/2020-12-04T21:22:38+00:00_5913788595.fit'
+directory = cwd + '/data/garmin_backup/'
+filename = directory + '/data/garmin_backup/2020-12-04T21:22:38+00:00_5913788595.fit'
 # '2020-12-04T21:22:38+00:00_5913788595.fit'
 
-fitfile = FitFile(file)
+file_list = [filename1 for filename1 in os.listdir(directory) if filename1.endswith(".fit") ]
 
-# Get all data messages that are of type record
-# include:
-# exclude: file_id,'file_creator','unknown','device_settings','user_profile','zones_target'
-fit_record_type = ['record', 'hrv' ,'event'] #, 'sport']
-workout_id = file[-14:-4]
-dict_df_workouts = {}
+for file in file_list:
 
-for record_type in fit_record_type:
-    list_workouts = []
-    list_labels = ['start', 'end']
+    fitfile = FitFile(filename)
 
-    for record in fitfile.get_messages(record_type):
-        # Go through all the data entries in this record
-        workout_dict = {}
-        for record_data in record:
+    # Get all data messages that are of type record
+    # include:
+    # exclude: file_id,'file_creator','unknown','device_settings','user_profile','zones_target'
+    fit_record_type = ['record', 'hrv' ,'event'] #, 'sport']
+    workout_id = filename[-14:-4]
+    dict_df_workouts = {}
 
-            # # Print the records name and value (and units if it has any)
-            # if record_data.units and 'unknown' not in record_data.name:
-            #     print(" * %s: %s %s" % (
-            #         record_data.name, record_data.value, record_data.units))
-            #
-            #     if isinstance(record_data.value, (list,tuple)):
-            #
-            #         for item, label  in zip(record_data.value, list_labels):
-            #             workout_dict[record_data.name + '_' + label] = item
-            #     else:
-            #         workout_dict[record_data.name] = record_data.value
+    for record_type in fit_record_type:
+        list_workouts = []
+        list_labels = ['start', 'end']
 
-            if 'unknown' not in record_data.name:
-                # print(" * %s: %s" % (record_data.name, record_data.value))
+        for record in fitfile.get_messages(record_type):
+            # Go through all the data entries in this record
+            workout_dict = {}
+            for record_data in record:
 
-                if isinstance(record_data.value, (list, tuple)) and record_type == record:
-                    for item, label in zip(record_data.value, list_labels):
-                        workout_dict[record_data.name + '_' + label] = item
-                else:
-                    workout_dict[record_data.name] = record_data.value
+                if 'unknown' not in record_data.name:
+                    # print(" * %s: %s" % (record_data.name, record_data.value))
 
-        workout_dict['workout ID'] = workout_id
-        list_workouts.append(workout_dict)
-        # print()
+                    if isinstance(record_data.value, (list, tuple)) and record_type == record:
+                        for item, label in zip(record_data.value, list_labels):
+                            workout_dict[record_data.name + '_' + label] = item
+                    else:
+                        workout_dict[record_data.name] = record_data.value
 
-    df_workouts = pd.DataFrame(list_workouts)
-    df_workouts.name = record_type
-    dict_df_workouts = ggh.df_create_append(df_workouts, dict_df_workouts, record_type)
+            workout_dict['workout ID'] = workout_id
+            list_workouts.append(workout_dict)
+            # print()
+
+        df_workouts = pd.DataFrame(list_workouts)
+        df_workouts.name = record_type
+        dict_df_workouts = ggh.df_create_append(df_workouts, dict_df_workouts, record_type)
 print(dict_df_workouts)
 
 # Save all data files to a sub directory to avoid cluttering
