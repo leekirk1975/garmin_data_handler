cabef894b3586b23a1075f7f48b5371df0d35afa
leekirk1975 <leekirk1975@me.com>
2021-01-16 15:07:36 +0800

    add fit to panada

--- convert_garmin_fit_ to_csv.py
+++ convert_garmin_fit_ to_csv.py
@@ -4,63 +4,72 @@ import pandas as pd
 # import datetime
 import os
 import utilities as util
+import traceback
 
 # dict to store all dataframes for export to csv
 dict_df_workouts = {}
 
 # Create list of all files with a .fit extension
 cwd = os.getcwd()  # get the current working directory
-#directory = cwd + '/data/garmin_backup/test/'
-directory = cwd + '/data/garmin_backup/'
+directory = cwd + '/data/garmin_backup/test/'
+#directory = cwd + '/data/garmin_backup/'
 file_list = [filename for filename in os.listdir(directory) if filename.endswith(".fit")]
 
 # process all the fit files in the list
 for filename in file_list:
     this_file = directory + filename
-    fit_file = FitFile(this_file)
-
-    # include: record, hrv, event messages from the passed fit files
-    # exclude: file_id,'file_creator','unknown','device_settings','user_profile','zones_target','sport'
-    fit_record_type = ['record', 'hrv', 'event']  #
     # extract the workout ID from the fit file name
     workout_id = filename[-14:-4]
     print(workout_id)
 
+    # include: record, hrv, event messages from the passed fit files
+    # exclude: file_id,'file_creator','unknown','device_settings','user_profile','zones_target','sport'
+    fit_record_type = ['record', 'hrv', 'event']  #
 
-    # get the sport of the current activity from the fit file
-    this_sport = None
-    for sport in fit_file.get_messages('sport'):
-        this_sport = DataMessage.get_value(sport, 'sport')
+    # create fit object
+    try: #handle ny problems with fit files
+        fit_file = FitFile(this_file)
 
-    # only process fit files with cycling data (indoor or outdoor)
-    if this_sport == 'cycling':
+        # get the sport of the current activity from the fit file
+        this_sport = None
+        for sport in fit_file.get_messages('sport'):
+            while sport == None:
+                this_sport = DataMessage.get_value(sport, 'sport')
 
-        for record_type in fit_record_type:
-            list_workouts = []
-            list_labels = ['start', 'end']
+        # only process fit files with cycling data (indoor or outdoor)
+        if this_sport == 'cycling':
 
-            for record in fit_file.get_messages(record_type):
-                # Go through all the data entries in this record
-                workout_dict = {}
-                for record_data in record:
+            for record_type in fit_record_type:
+                list_workouts = []
+                list_labels = ['start', 'end']
 
-                    if 'unknown' not in record_data.name:  # exclude in field labelled unknown
-                        #print(" * %s: %s" % (record_data.name, record_data.value))  # split power phase start and end records into two fields
-                        if isinstance(record_data.value, (list, tuple)) and record_type == 'record':
-                            for item, label in zip(record_data.value, list_labels):
-                                workout_dict[record_data.name + '_' + label] = item
-                        else:
-                            workout_dict[record_data.name] = record_data.value
+                for record in fit_file.get_messages(record_type):
+                    # Go through all the data entries in this record
+                    workout_dict = {}
+                    for record_data in record:
 
-                workout_dict['workout ID'] = workout_id
-                list_workouts.append(workout_dict)
-                # print()
+                        if 'unknown' not in record_data.name:  # exclude in field labelled unknown
+                            #print(" * %s: %s" % (record_data.name, record_data.value))  # split power phase start and end records into two fields
+                            if isinstance(record_data.value, (list, tuple)) and record_type == 'record':
+                                for item, label in zip(record_data.value, list_labels):
+                                    workout_dict[record_data.name + '_' + label] = item
+                            else:
+                                workout_dict[record_data.name] = record_data.value
 
-            df_workouts = pd.DataFrame(list_workouts)
-            df_workouts.name = record_type
-            dict_df_workouts = util.df_create_append(df_workouts, dict_df_workouts, record_type)
+                    workout_dict['workout ID'] = workout_id
+                    list_workouts.append(workout_dict)
+                    # print()
+
+                df_workouts = pd.DataFrame(list_workouts)
+                df_workouts.name = record_type
+                dict_df_workouts = util.df_create_append(df_workouts, dict_df_workouts, record_type)
+    except Exception:
+        traceback.print_exc()
+        print('There was an error with workout: '+ workout_id )
 print(dict_df_workouts)
 
 # Save all data to csv files
 for df_item in dict_df_workouts.keys():
     util.df_to_csv(cwd + '/data/', dict_df_workouts[df_item], df_item, ' raw data.csv')
+
+print('finished wrting csv files')
