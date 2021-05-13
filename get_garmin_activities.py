3da2937919029d47045f48dcade6f08aca9f3b92
leekirk1975 <leekirk1975@me.com>
2021-05-13 20:23:40 +0800

    Commit working implementation of user agent

--- get_garmin_activities.py
+++ get_garmin_activities.py
@@ -1,14 +1,22 @@
 """Export activities from garmin that are not in the dir, in all formats (json, gpx, tcx, FIT)"""
 
-#For library clients, one can pass a user_agent_fn function that, when called, produces a
-#User-Agent string to be used as User-Agent for the remainder of the session.
-
 import garminexport.incremental_backup as gcb
 import os
+from fake_useragent import UserAgent
+
+
+#created a randam user agent
+#For library clients, one can pass a user_agent_fn function that, when called, produces a
+#User-Agent string to be used as User-Agent for the remainder of the session.
+def gen_user_agent():
+    ua = UserAgent()
+    return ua.random
 
 print("logging on")
 # get login and password
 email, pwd = open('GarminDetails.txt').read().strip().split(',')
+
+
 
 """Use Garminexport to export all activity history from garmin connect, 
 then set the location and copy all files to this location"""
@@ -17,5 +25,5 @@ backup_dir = cwd + '/data/garmin_backup'
 # write all files types to the back-up directory - Json, gpx, tcx and FIT
 print(backup_dir)
 print('writing back-up files')
-gcb.incremental_backup(email, pwd, backup_dir)
+gcb.incremental_backup(email, pwd, gen_user_agent, backup_dir)
 print('all done')
